2ecb2bbe68b2b730c564e64bbad03f18ca28c986
bezzzumen <113475675+bezzzumen@users.noreply.github.com>
2026-08-11 09:28:08 +0300

    Update database.js

--- database.js
+++ database.js
@@ -20,7 +20,7 @@ const seedData = require('./seed-data');
 const CATEGORIES_META = [
   { key: 'garnish', name: 'Гарнір', emoji: '🌾', target_calories: 360 },
   { key: 'dairy', name: 'Молочні продукти', emoji: '🥛', target_calories: 260 },
-  { key: 'freebie', name: '3) Будь-чого (~425 Ккал)', emoji: '🍫', target_calories: 425 },
+  { key: 'freebie', name: 'Будь-чого', emoji: '🍫', target_calories: 425 },
   { key: 'protein', name: "М'ясо / Риба / Яйця", emoji: '🍗', target_calories: 400 },
   { key: 'veggies', name: 'Овочі та гриби', emoji: '🥦', target_calories: 120 },
   { key: 'fats', name: 'Жири та соуси', emoji: '🥑', target_calories: 220 },
